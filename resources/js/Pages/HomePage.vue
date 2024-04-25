c8d3ef944b0977c292c5f5a12345b14b72c2655f
mzaky <mzakyf5503@gmail.com>
2024-04-25 11:03:59 +0700

    formatting

--- resources/js/Pages/HomePage.vue
+++ resources/js/Pages/HomePage.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
-    <h1>HOME page dokter</h1>
-    <router-link to="/test"> Take me to Test page </router-link>
-    <button @click.prevent="getValue">Trigger Endpoint</button>
-    <p v-if="response">
-      {{ response.length != 0 ? response[0].name : "user kosong" }}
-    </p>
-  </div>
+    <div>
+        <h1>HOME page dokter</h1>
+        <router-link to="/test"> Take me to Test page </router-link>
+        <button @click.prevent="getValue">Trigger Endpoint</button>
+        <p v-if="response">
+            {{ response.length != 0 ? response[0].name : "user kosong" }}
+        </p>
+    </div>
 </template>
 
 <script setup>
@@ -16,14 +16,13 @@ import { ref } from "vue";
 const response = ref("");
 
 const getValue = async () => {
-  try {
-    const res = await axios.get("/api/users");
-    response.value = res.data;
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        const res = await axios.get("/api/users");
+        response.value = res.data;
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 
 </script>
-

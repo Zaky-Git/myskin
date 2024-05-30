906f9996a4ee4f43d6d7c70a30c9c29ce64727a6
SuryaaAulia <suryaaulia489@gmail.com>
2024-05-30 22:27:22 +0700

    Upadate - AdminLayout
    Menambahkan element untuk menampilkan page

--- react/src/components/AdminLayout.jsx
+++ react/src/components/AdminLayout.jsx
@@ -1,5 +1,15 @@
+import {NavigationBarAdminDoctor} from "./NavigationBar.jsx";
+import {Outlet} from "react-router-dom";
+
 const AdminLayout = () => {
-    return <div>AdminLayout</div>;
+    return (
+        <div className="flex flex-col min-h-screen bg-primaryAlternativeTW font-poppins">
+            <NavigationBarAdminDoctor/>
+            <div className="flex-grow">
+                <Outlet/>
+            </div>
+        </div>
+    );
 };
 
 export default AdminLayout;

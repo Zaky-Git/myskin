9faac8e96739f205862fbea34b56ff0d8b86ca68
SuryaaAulia <suryaaulia489@gmail.com>
2024-05-30 21:58:59 +0700

    Create - RiwayatVerifikasi
    Menambahkan tampilan page RiwayatVerifikasi

---
+++ react/src/components/RiwayatVerifikasi.jsx
@@ -0,0 +1,78 @@
+const RiwatVerifikasi = () => {
+    const data = [
+        {
+            Tanggal: "22/03/2024",
+            Pasien: "Hasnan Surya",
+            DianosisAI: "Lentigo Maligma",
+            DianosisDokter: "Lentigo Maligma",
+            Catatan: "Lorem Ipsum dolor Sit amet"
+        },
+        {
+            Tanggal: "22/03/2024",
+            Pasien: "Hasnan Surya",
+            DianosisAI: "Lentigo Maligma",
+            DianosisDokter: "Lentigo Maligma",
+            Catatan: "Lorem Ipsum dolor Sit amet"
+        },
+        {
+            Tanggal: "22/03/2024",
+            Pasien: "Hasnan Surya",
+            DianosisAI: "Lentigo Maligma",
+            DianosisDokter: "Lentigo Maligma",
+            Catatan: "Lorem Ipsum dolor Sit amet"
+        },
+        {
+            Tanggal: "22/03/2024",
+            Pasien: "Hasnan Surya",
+            DianosisAI: "Lentigo Maligma",
+            DianosisDokter: "Lentigo Maligma",
+            Catatan: "Lorem Ipsum dolor Sit amet"
+        },
+        {
+            Tanggal: "22/03/2024",
+            Pasien: "Hasnan Surya",
+            DianosisAI: "Lentigo Maligma",
+            DianosisDokter: "Lentigo Maligma",
+            Catatan: "Lorem Ipsum dolor Sit amet"
+        },
+    ];
+
+    return (
+        <div className="dashboard-content">
+            <div className="card-custom shadow-xl p-3 mt-4">
+                <h3 className="font-bold">
+                    Pasien
+                    <hr/>
+                </h3>
+                <table className="table table-hover">
+                    <thead>
+                    <tr>
+                        <th className="col-2">Tanggal Daftar</th>
+                        <th className="col-2">Nama Lengkap</th>
+                        <th className="col-2">Email</th>
+                        <th className="col-2">Nomor Telepon</th>
+                        <th className="col-2">Umur</th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {data.map((item, index) => (
+                        <tr key={index}>
+                            <td>{item.Tanggal}</td>
+                            <td>{item.Pasien}</td>
+                            <td>{item.DianosisAI}</td>
+                            <td>{item.DianosisDokter}</td>
+                            <td>{item.Catatan}</td>
+                            <td>
+                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Detail </button>
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+}
+
+export default RiwatVerifikasi;

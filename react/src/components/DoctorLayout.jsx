12b8851511c63e4358c50472bd3f464d003021fd
SuryaaAulia <suryaaulia489@gmail.com>
2024-05-30 22:27:38 +0700

    Upadate - DokterLayout
    Menambahkan element untuk menampilkan page

--- react/src/components/DoctorLayout.jsx
+++ react/src/components/DoctorLayout.jsx
@@ -1,10 +1,15 @@
-import { NavigationBar } from "./NavigationBar";
+import {NavigationBarAdminDoctor} from "./NavigationBar.jsx";
+import {Outlet} from "react-router-dom";
 
 const DoctorLayout = () => {
     return (
-        <div>
-            <NavigationBar />
+        <div className="flex flex-col min-h-screen bg-primaryAlternativeTW font-poppins">
+            <NavigationBarAdminDoctor/>
+            <div className="flex-grow">
+                <Outlet/>
+            </div>
         </div>
+
     );
 };
 
